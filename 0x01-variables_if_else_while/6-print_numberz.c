a3fdd75affe887ac7031d03a45f41fc1f4f5e146
Akinlolu-Felicia <akinlolufelicia778@gmail.com>
2022-09-09 09:38:55 -0700

    If else task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * prints single numbers in base ten
+ *
+ * Return: 0
+ */
+int main(void)
+{	char c;
+	c = '0';
+	while loop(c <= '9')
+	{	putchar(c)
+		c++;
+	}
+	putchar('\n')
+	return (0);
+}
