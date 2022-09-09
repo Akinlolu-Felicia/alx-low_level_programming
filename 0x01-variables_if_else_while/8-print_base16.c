e9c87bbcd4a2b44e93a5443b8a3bd07c69ba154e
Akinlolu-Felicia <akinlolufelicia778@gmail.com>
2022-09-09 13:18:23 -0700

    If else task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * prints hexadecimal numbers
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{	int i, y;
+	i = '0';
+	y = 'a';
+
+	for (i < 10; i++;)
+	{
+		putchar(i);
+	}
+	for (y <= f; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
