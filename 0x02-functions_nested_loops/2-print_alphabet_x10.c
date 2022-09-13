c856a28299572eb34110aad79724ab5114401d1d
Akinlolu-Felicia <akinlolufelicia778@gmail.com>
2022-09-13 12:01:40 -0700

    Nested loop task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the lowercase alphabet ten times on new lines
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+		}
+		_putchar(10);
+	}
+}
