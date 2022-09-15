81933e29d379ad538c9c7f39127f0fd6c626eacc
Akinlolu-Felicia <akinlolufelicia778@gmail.com>
2022-09-15 06:51:53 -0700

    Nested loop 2 task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints integers from 0 - 9
+ *
+ * Return: void
+ *
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c >= 48 && c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar(10);
+
+}
