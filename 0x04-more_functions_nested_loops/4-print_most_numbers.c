378644d811da078796141f8de516cf70eb990bf6
Akinlolu-Felicia <akinlolufelicia778@gmail.com>
2022-09-15 06:59:27 -0700

    Nested loop 2 task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints integers from 0 - 9 except 2 and 4
+ *
+ * Return: void
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c >= 48 && c <= 57 && c != 2 && c != 4; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar(10);
+}
