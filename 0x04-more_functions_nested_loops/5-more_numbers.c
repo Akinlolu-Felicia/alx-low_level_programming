0c1f77fe4a7a2b3fd44c5643b36a652cb8aa3519
Akinlolu-Felicia <akinlolufelicia778@gmail.com>
2022-09-15 07:09:29 -0700

    Nested loop 2 task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - prints integers from 0 - 14
+ *
+ * Return: void
+ *
+ */
+
+void more_numbers(void)
+{
+	int c;
+
+	for (c = 48; c >= 48 && c <= 62; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar(10);
+}
+
