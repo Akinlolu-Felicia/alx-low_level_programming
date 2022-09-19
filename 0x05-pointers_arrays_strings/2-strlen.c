48b36bdb04990dd1454747df8be7ca93acfffad3
Akinlolu-Felicia <akinlolufelicia778@gmail.com>
2022-09-19 04:41:06 -0700

    pointers task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string whose length to be returned
+ *
+ * Return: 0
+ *
+ */
+
+int _strlen(char *s)
+{
+	strlen(s);
+	return (0);
+}
