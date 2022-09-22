3c8fbb1b7fbbeac98797d91f651280b72cec9a5c
Akinlolu-Felicia <akinlolufelicia778@gmail.com>
2022-09-22 03:36:26 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,15 +4,11 @@
  * @str: string
  * Return: returns char
  */
-
-
 char *string_toupper(char *str)
 {
 int i;
-i = 0;
 
-while (str[i] != '\0')
-	i++;
+for (i = 0; str[i] != '\0'; i++)
 {
 if (str[i] >= 'a' && str[i] <= 'z')
 {
