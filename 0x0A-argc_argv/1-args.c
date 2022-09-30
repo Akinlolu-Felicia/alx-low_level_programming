586c826380098fe7579fc8032e23f127cc826943
Akinlolu-Felicia <akinlolufelicia778@gmail.com>
2022-09-30 00:22:14 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * @argc: params
+ *
+ * @argv: params
+ *
+ * Return: 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i > 1; i++)
+	{
+		printf("%s \n", argv[i]);
+	}
+	return (0);
+}
